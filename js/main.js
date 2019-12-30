6b69990c5e31457abeb0392ddcafe07f62598ee2
Omor Faruk <46064292+nkomorfaruk@users.noreply.github.com>
2019-12-31 01:45:55 +0600

    secound commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+"use strict";
+
+    // progress bar 
+
+    $(document).ready(function(){
+        $('#bar1').barfiller({barColor: "#155bc5", duration: 1000});
+        $('#bar2').barfiller({barColor: "#155bc5", duration: 1000});
+        $('#bar3').barfiller({barColor: "#155bc5", duration: 1000});
+        $('#bar4').barfiller({barColor: "#155bc5", duration: 1000});
+        $('#bar5').barfiller({barColor: "#155bc5", duration: 1000});
+    });
+
+    
+
+
+})(jQuery);
